9cc23c6278a4acf34b51f5dc62cfebdb8c100dca
danlebedev <lebedev.py@gmail.com>
2023-05-10 10:37:49 +0300

    rename bot.py & placed token out

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+import config
+
+
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+bot = Bot(token=config.token)
 # Диспетчер
 dp = Dispatcher()
 
